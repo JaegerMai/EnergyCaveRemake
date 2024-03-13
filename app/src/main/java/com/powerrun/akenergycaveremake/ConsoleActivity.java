a1beb995dddbccc353e1d87cb0b983020d472227
Jaeger Mai <1943715063@qq.com>
2024-03-13 12:02:49 +0800

    完成consoleActivity的UI交互和框架搭建

--- app/src/main/java/com/powerrun/akenergycaveremake/ConsoleActivity.java
+++ app/src/main/java/com/powerrun/akenergycaveremake/ConsoleActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
@@ -16,25 +21,83 @@ import android.widget.Toast;
 
 import com.powerrun.akenergycaveremake.common.BaseActivity;
 
+import java.io.File;
+import java.util.HashMap;
+
 public class ConsoleActivity extends BaseActivity implements View.OnClickListener, View.OnLongClickListener {
     private static final String TAG = "ConsoleActivity";
     private Context mContext;
-    private TextView tvTime;
-    private TextView tvTemp0;
-    private TextView tvTemp1;
-    private ImageView ivCurrentTemp0;
-    private ImageView ivCurrentTemp1;
     private MusicHelper musicHelper;
+    private Handler exitLongPressHandler = new Handler();
+    private static final String FONT_DIGITAL_7 = "raw" + File.separator
+            + "digital-7.ttf";
+    //按钮点击和长按事件,使用HashMap存储
+    private HashMap<Integer,Runnable> clickActions = new HashMap<>();
+    private HashMap<Integer,Runnable> longPressActions = new HashMap<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_console);
         mContext = this;
+        //初始化音乐播放器
         musicHelper = new MusicHelper();
         musicHelper.create(mContext);
+        //初始化UI
         initUI();
+        //TODO: 初始化蓝牙
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        musicHelper.destroy();
+        findViewById(R.id.image_button_music).clearAnimation();
+        //TODO: 关闭蓝牙
+    }
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        //从HashMap获取并执行对应的操作
+        Runnable action = clickActions.get(id);
+        if(action != null) {
+            action.run();
+        }
     }
 
+    @Override
+    public boolean onLongClick(View view) {
+        // TODO: 实现长按逻辑
+        Toast.makeText(getApplicationContext(), "长按了"+view.getId(), Toast.LENGTH_SHORT).show();
+        return true;
+    }
+    /**
+     * 重写返回键，退出时弹出提示框
+     */
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        showExitDialog();
+    }
+    /**
+     * 根据传感器返回的数据修改温度显示
+     * 分别在30,35,40,45度时显示不同的图标
+     */
+    void updateTempDisplay(ImageView iv, int temp) {
+        int[] tempIcons = {
+                R.drawable.icon_console_weatherglass_1_39_104,
+                R.drawable.icon_console_weatherglass_2_39_104,
+                R.drawable.icon_console_weatherglass_3_39_104,
+                R.drawable.icon_console_weatherglass_4_39_104,
+                R.drawable.icon_console_weatherglass_5_39_104
+        };
+        // 30-45度之间显示不同的图标
+        int index = temp / 5;
+        if (index < 0) {
+            index = 0;
+        } else if (index > 4) {
+            index = 4;
+        }
+        iv.setImageResource(tempIcons[index]);
+    }
     /**
      * 打开音乐选择对话框
      */
@@ -53,69 +116,118 @@ public class ConsoleActivity extends BaseActivity implements View.OnClickListene
                 });
         builder.create().show();
     }
-    @Override
-    public void onClick(View view) {
-        if (view.getId() == R.id.image_button_music) {
-            openMusicDialog();
+    //退出按钮长按标志
+    private boolean isExitLongPress = false;
+    /**
+     * 退出按钮长按监听
+     * 长按6秒会清楚设备连接并退出程序
+     * 短按会弹出退出提示框
+     */
+    private View.OnTouchListener exitLongPressListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            switch (motionEvent.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    isExitLongPress = false;
+                    exitLongPressHandler.postDelayed(exitLongPressRunnable, 6000);//长按六秒后执行
+                    break;
+                case MotionEvent.ACTION_UP:
+                    if(!isExitLongPress) {
+                        exitLongPressHandler.removeCallbacks(exitLongPressRunnable);
+                        showExitDialog();
+                    }
+                    break;
+            }
+            return true;
         }
-    }
-
-    @Override
-    public boolean onLongClick(View view) {
-        // TODO: 实现长按逻辑
-        Toast.makeText(getApplicationContext(), "长按了"+view.getId(), Toast.LENGTH_SHORT).show();
-        return false;
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        musicHelper.destroy();
-        findViewById(R.id.image_button_music).clearAnimation();
-    }
+    };
+    //退出按钮长按事件
+    private Runnable exitLongPressRunnable = new Runnable() {
+        @Override
+        public void run() {
+            Log.i(TAG, "长按退出程序");
+            isExitLongPress = true;
+            AlertDialog.Builder builder = new AlertDialog.Builder(mContext)
+                    .setTitle("警告")
+                    .setMessage("是否要清除与该设备的链接并退出？")
+                    .setNegativeButton("取消", (dialog, which) -> {
+                        dialog.dismiss();
+                    })
+                    .setPositiveButton("确定", (dialog, which) -> {
+                        //TODO: 清除蓝牙设备连接，并关机
+                        dialog.dismiss();
+                        finish();
+                    });
+            builder.create().show();
+        }
+    };
 
     /**
      * 初始化UI
      */
     private void initUI() {
         // 获取控件
-        ImageButton imgBtnTimeAdd = findViewById(R.id.image_button_time_add);
-        ImageButton imgBtnTimeDec = findViewById(R.id.image_button_time_dec);
-        ImageButton imgBtnTempAdd0 = findViewById(R.id.image_button_temp_add_0);
-        ImageButton imgBtnTempDec0 = findViewById(R.id.image_button_temp_dec_0);
-        ImageButton imgBtnTempAdd1 = findViewById(R.id.image_button_temp_add_1);
-        ImageButton imgBtnTempDec1 = findViewById(R.id.image_button_temp_dec_1);
-        ImageButton imgBtnSwitch   = findViewById(R.id.image_button_switch);
-        ImageButton imgBtnExit     = findViewById(R.id.image_button_exit);
-        ImageButton imgBtnMusic    = findViewById(R.id.image_button_music);
-        tvTime = findViewById(R.id.text_view_time);
-        tvTemp0 = findViewById(R.id.text_view_temp_0);
-        tvTemp1 = findViewById(R.id.text_view_temp_1);
-        ivCurrentTemp0 = findViewById(R.id.image_view_temp_0);
-        ivCurrentTemp1 = findViewById(R.id.image_view_temp_1);
-        // 设置旋转动画
+        ImageButton[] buttons = new ImageButton[]{
+                findViewById(R.id.image_button_time_add),
+                findViewById(R.id.image_button_time_dec),
+                findViewById(R.id.image_button_temp_add_0),
+                findViewById(R.id.image_button_temp_dec_0),
+                findViewById(R.id.image_button_temp_add_1),
+                findViewById(R.id.image_button_temp_dec_1),
+                findViewById(R.id.image_button_power),
+                findViewById(R.id.image_button_exit),
+                findViewById(R.id.image_button_music)
+        };
+        TextView[] textViews = new TextView[]{
+                findViewById(R.id.text_view_time),
+                findViewById(R.id.text_view_temp_0),
+                findViewById(R.id.text_view_temp_1)
+        };
+
+        // 设置点击事件
+        for (ImageButton button : buttons) {
+            button.setOnClickListener(this);
+            button.setOnLongClickListener(this);
+        }
+        findViewById(R.id.image_button_exit).setOnTouchListener(exitLongPressListener);
+
+        // 为每个按钮ID添加对应的操作
+        clickActions.put(R.id.image_button_music, this::openMusicDialog);
+        // TODO: 添加其他按钮的点击事件
+
+        // 设置字体样式
+        AssetManager assets = getApplication().getAssets();
+        final Typeface font = Typeface.createFromAsset(assets, FONT_DIGITAL_7);
+        for (TextView textView : textViews) {
+            textView.setTypeface(font);
+        }
+
+        // 设置音乐按钮旋转动画
         Animation rotateAnimation  = new RotateAnimation(0f, 360f,
                 Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
         rotateAnimation.setFillAfter(true);
         rotateAnimation.setDuration(3000);
         rotateAnimation.setRepeatCount(-1);
         rotateAnimation.setInterpolator(new LinearInterpolator());
-        imgBtnMusic.startAnimation(rotateAnimation);
-        // 设置点击事件
-        imgBtnTimeAdd.setOnClickListener(this);
-        imgBtnTimeDec.setOnClickListener(this);
-        imgBtnTempAdd0.setOnClickListener(this);
-        imgBtnTempDec0.setOnClickListener(this);
-        imgBtnTempAdd1.setOnClickListener(this);
-        imgBtnTempDec1.setOnClickListener(this);
-        imgBtnSwitch.setOnClickListener(this);
-        imgBtnExit.setOnClickListener(this);
-        imgBtnMusic.setOnClickListener(this);
-        imgBtnTimeAdd.setOnLongClickListener(this);
-        imgBtnTimeDec.setOnLongClickListener(this);
-        imgBtnTempAdd0.setOnLongClickListener(this);
-        imgBtnTempDec0.setOnLongClickListener(this);
-        imgBtnTempAdd1.setOnLongClickListener(this);
-        imgBtnTempDec1.setOnLongClickListener(this);
+        findViewById(R.id.image_button_music).startAnimation(rotateAnimation);
+        //TODO: 蓝牙图标，显示连接状态
+    }
+    /**
+     * 退出提示框
+     */
+    private void showExitDialog() {
+        Log.i(TAG, "短按退出程序");
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext)
+                .setTitle("警告")
+                .setMessage("确定要退出吗？")
+                .setPositiveButton("确定", (dialog, which) -> {
+                    //TODO: 蓝牙发送推出指令
+                    dialog.dismiss();
+                    finish();
+                })
+                .setNegativeButton("取消", (dialog, which) -> {
+                    dialog.dismiss();
+                });
+        builder.create().show();
     }
 }
---
+++ app/src/main/java/com/powerrun/akenergycaveremake/MyMessage.java
@@ -0,0 +1,35 @@
+package com.powerrun.akenergycaveremake;
+
+public class MyMessage {
+    public static final String SHDN_CODE = "a";          //开/关机
+    public static final String STOP_CODE = "b";          //继续/暂停
+    public static final String PW_ADD_CODE_0 = "c";  //通道0温度加码 上舱
+    public static final String PW_DEC_CODE_0 = "d";  //通道0温度减码 上舱
+    public static final String PW_ADD_CODE_1 = "e";  //通道1温度加码 e    下舱
+    public static final String PW_DEC_CODE_1 = "f";  //通道1温度减码 d    下舱
+    public static final String TM_ADD_CODE = "g";      //时间加10码
+    public static final String TM_DEC_CODE = "h";     //时间减10码
+    public static final String OLD_TM_ADD_CODE = "w";      //时间加10码
+    public static final String OLD_TM_DEC_CODE = "x";     //时间减10码
+    public static final String SENT = "i";               //发送温度、时间的查询
+
+    /**---------------------------------------------------**/
+    public static final String T_ADD_CH_0 = "l";          //10.ch0温度设定增：l
+    public static final String T_DEC_CH_0 = "m";          //11.ch0温度设定减：m
+    public static final String T_ADD_CH_1 = "n";          //12.ch1温度设定增：n
+    public static final String T_DEC_CH_1 = "o";          //13.ch1温度设定减：o
+
+    public static final int PW_ch0_MAX = 60;
+    public static final int PW_ch0_MIN = 30;
+    public static final int PW_ch1_MAX = 60;
+    public static final int PW_ch1_MIN = 30;
+
+    /**---------------------------------------------------**/
+
+    public static final int PW_0_MAX = 5;
+    public static final int PW_0_MIN = 0;
+    public static final int PW_1_MAX = 5;
+    public static final int PW_1_MIN = 0;
+    public static final int TM_MAX = 90;
+    public static final int TM_MIN = 0;
+}
